c8adf9011a34659327b04ae724db88b326db4b8a
Francois Gervais <francoisgervais@gmail.com>
2023-07-08 09:39:48 -0400

    Remove console suspend

--- app/src/main.c
+++ app/src/main.c
@@ -14,16 +14,10 @@ LOG_MODULE_REGISTER(main, LOG_LEVEL_DBG);
 void main(void)
 {
 	int ret;
-	const struct device *cons = DEVICE_DT_GET(DT_CHOSEN(zephyr_console));
 
 	LOG_INF("Version: %s", APP_VERSION_FULL);
 
 	LOG_INF("****************************************");
 	LOG_INF("MAIN DONE");
 	LOG_INF("****************************************");
-
-	k_sleep(K_SECONDS(3));
-	// pm_device_action_run(cons, PM_DEVICE_ACTION_SUSPEND);
-
-	LOG_INF("PM_DEVICE_ACTION_SUSPEND");
 }
