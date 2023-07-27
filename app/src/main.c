bd193e3451b734c2f9a0368e3cf3f7d3aebe2d8d
Francois Gervais <francoisgervais@gmail.com>
2023-07-27 18:45:49 -0400

    Instantiate the co2 sensor

--- app/src/main.c
+++ app/src/main.c
@@ -13,15 +13,76 @@ LOG_MODULE_REGISTER(main, LOG_LEVEL_DBG);
 #include "openthread.h"
 
 
+static int get_scd4x_serial_as_string(hvac_t *hvac_ctx,
+				      char *sn_buf, size_t sn_buf_size)
+{
+	int ret;
+	uint16_t scd4x_serial_words[3];
+
+	hvac_scd40_get_serial_number(hvac_ctx, scd4x_serial_words);
+
+	ret = snprintf(sn_buf, sn_buf_size,
+		       "%04x%04x%04x",
+		       scd4x_serial_words[0],
+		       scd4x_serial_words[1],
+		       scd4x_serial_words[2]);
+	if (ret < 0 && ret >= sn_buf_size) {
+		LOG_ERR("Could not set sn_buf");
+		return -ENOMEM;
+	}
+
+	LOG_INF("SCD4x serial number: %s", sn_buf);
+	return 0;
+}
+
+static int get_sps30_serial_as_string(hvac_t *hvac_ctx,
+				      char *sn_buf, size_t sn_buf_size)
+{
+	int ret;
+
+	ret = hvac_sps30_get_serial_number(hvac_ctx, sn_buf, sn_buf_size);
+
+	LOG_INF("SPS30 serial number: %s", sn_buf);
+	return 0;
+}
+
+
+static int generate_unique_id(char *uid_buf, size_t uid_buf_size,
+			      const char *part_number,
+			      const char *sensor_name,
+			      const char *serial_number)
+{
+	int ret;
+
+	ret = snprintf(uid_buf, uid_buf_size,
+		       "%s_%s_%s",
+		       part_number, serial_number, sensor_name);
+	if (ret < 0 && ret >= uid_buf_size) {
+		LOG_ERR("Could not set uid_buf");
+		return -ENOMEM;
+	}
+
+	LOG_INF("📇 unique id: %s", uid_buf);
+	return 0;
+}
+
 int main(void)
 {
 	hvac_t hvac;
 	hvac_cfg_t hvac_cfg;
 
 	int ret;
-	uint16_t scd4x_serial_words[3];
 	char scd4x_serial_string[128];
 	char sps30_serial_string[HVAC_SPS30_MAX_SERIAL_LEN];
+	char scd4x_co2_unique_id_string[128];
+	char sps30_pm25_unique_id_string[128];
+
+	struct ha_sensor co2_sensor = {
+		.name = "CO₂",
+		.unique_id = scd4x_co2_unique_id_string,
+		.device_class = "carbon_dioxide",
+		.state_class = "measurement",
+	};
 
 	LOG_INF("\n\n🐨 MAIN START 🐨\n");
 
@@ -39,27 +100,42 @@ int main(void)
 	LOG_INF("Version: %s", APP_VERSION_FULL);
 
 	hvac_init(&hvac, &hvac_cfg);
-	hvac_scd40_get_serial_number(&hvac, scd4x_serial_words);
-
-	ret = snprintf(scd4x_serial_string, sizeof(scd4x_serial_string),
-		       "%04x%04x%04x",
-		       scd4x_serial_words[0],
-		       scd4x_serial_words[1],
-		       scd4x_serial_words[2]);
-	if (ret < 0 && ret >= sizeof(scd4x_serial_string)) {
-		LOG_ERR("Could not set scd4x_serial_string");
-		return -ENOMEM;
+	ret = get_scd4x_serial_as_string(&hvac, scd4x_serial_string,
+					 sizeof(scd4x_serial_string));
+	if (ret < 0) {
+		LOG_ERR("Could not get scd4x serial number");
+		return ret;
 	}
 
-	LOG_INF("SCD4x - Serial Number : %s", scd4x_serial_string);
+	ret = get_sps30_serial_as_string(&hvac,
+					 sps30_serial_string,
+					 sizeof(sps30_serial_string));
+	if (ret < 0) {
+		LOG_ERR("Could not get sps30 serial number");
+		return ret;
+	}
 
-	hvac_sps30_get_serial_number(&hvac,
-		sps30_serial_string, sizeof(sps30_serial_string));
+	ret = generate_unique_id(scd4x_co2_unique_id_string,
+				 sizeof(scd4x_co2_unique_id_string),
+				 "scd4x", "co2",
+				 scd4x_serial_string);
+	if (ret < 0) {
+		LOG_ERR("Could not generate scd4x unique id");
+		return ret;
+	}
 
-	LOG_INF("SPS30 - Serial Number : %s", sps30_serial_string);
+	ret = generate_unique_id(sps30_pm25_unique_id_string,
+				 sizeof(sps30_pm25_unique_id_string),
+				 "sps30", "pm25",
+				 sps30_serial_string);
+	if (ret < 0) {
+		LOG_ERR("Could not generate sps30 unique id");
+		return ret;
+	}
 
 	// COMMENT OUT FOR FIRST TEST
-	// ha_start(scd4x_serial_string, "");
+	// ha_start();
+	// int ha_register_sensor(&co2_sensor);
 	// --------------------------
 
 	LOG_INF("🏁 MAIN DONE 🏁");
