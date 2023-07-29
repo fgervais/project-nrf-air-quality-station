87cc5b8ac3d2385f0d1b2344ac6f50f241a55aa6
Francois Gervais <francoisgervais@gmail.com>
2023-07-29 09:40:32 -0400

    Add HDC3020x files

---
+++ app/src/drv_digital_out.h
@@ -0,0 +1,27 @@
+#ifndef DRV_DIGITAL_OUT_H_
+#define DRV_DIGITAL_OUT_H_
+
+#include <zephyr/device.h>
+#include <zephyr/drivers/i2c.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// https://github.com/MikroElektronika/mikrosdk_v2/blob/076ab1c6ce8d141ecd5aedfcba20d84338c3c59b/drv/lib/include/drv_digital_out.h#L61
+typedef enum
+{
+	DIGITAL_OUT_SUCCESS = 0,	   /*!< Success. */
+	DIGITAL_OUT_UNSUPPORTED_PIN = (-1) /*!< Error. */
+} digital_out_err_t;
+
+typedef struct
+{
+	const struct device *dev;
+} digital_out_t;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* DRV_DIGITAL_OUT_H_ */
