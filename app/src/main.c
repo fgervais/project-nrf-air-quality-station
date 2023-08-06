24fa031e7ef89b846191d009c890ca7426fe5f1b
Francois Gervais <francoisgervais@gmail.com>
2023-08-06 10:52:09 -0400

    Initialize watchdog channels

--- app/src/main.c
+++ app/src/main.c
@@ -1,5 +1,6 @@
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/drivers/hwinfo.h>
+#include <zephyr/drivers/watchdog.h>
 #include <zephyr/kernel.h>
 #include <zephyr/pm/device.h>
 
@@ -253,9 +254,68 @@ static int hvac_click_init(hvac_t *hvac)
 	return 0;
 }
 
+static int watchdog_new_channel(const struct device *wdt)
+{
+	int ret;
+
+	struct wdt_timeout_cfg wdt_config = {
+		.window.min = 0,
+		.window.max = (3 * 60 + 40) * MSEC_PER_SEC,
+		.callback = NULL,
+		.flags = WDT_FLAG_RESET_SOC,
+	};
+
+	ret = wdt_install_timeout(wdt, &wdt_config);
+	if (ret < 0) {
+		LOG_ERR("watchdog install error");
+	}
+
+	return ret;
+}
+
+static int watchdog_init(const struct device *wdt,
+			 int *main_channel_id, int *mqtt_channel_id)
+{
+	int ret;
+
+	if (!device_is_ready(wdt)) {
+		LOG_ERR("%s: device not ready", wdt->name);
+		return -ENODEV;
+	}
+
+	ret = watchdog_new_channel(wdt);
+	if (ret < 0) {
+		LOG_ERR("Could not create a new watchdog channel");
+		return ret;
+	}
+
+	*main_channel_id = ret;
+	LOG_INF("main watchdog channel id: %d", *main_channel_id);
+
+	ret = watchdog_new_channel(wdt);
+	if (ret < 0) {
+		LOG_ERR("Could not create a new watchdog channel");
+		return ret;
+	}
+
+	*mqtt_channel_id = ret;
+	LOG_INF("mqtt watchdog channel id: %d", *mqtt_channel_id);
+
+	ret = wdt_setup(wdt, WDT_OPT_PAUSE_HALTED_BY_DBG);
+	if (ret < 0) {
+		LOG_ERR("watchdog setup error");
+		return 0;
+	}
+
+	LOG_INF("🐶 watchdog started!");
+
+	return 0;
+}
+
 int main(void)
 {
 	int ret;
+	int watchdog_main_chan_id, watchdog_mqtt_chan_id;
 
 	temphum24_t temphum24;
 	hvac_t hvac;
