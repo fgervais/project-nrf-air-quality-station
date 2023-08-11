f916bc0541b3151cb9dc9ea541d750041bafac3a
Francois Gervais <francoisgervais@gmail.com>
2023-08-11 08:26:57 -0400

    Move watchdog to init module

---
+++ app/src/init.h
@@ -0,0 +1,2 @@
+int init_watchdog(const struct device *wdt,
+		  int *main_channel_id, int *mqtt_channel_id);
--- app/src/main.c
+++ app/src/main.c
@@ -12,6 +12,7 @@ LOG_MODULE_REGISTER(main, LOG_LEVEL_DBG);
 
 #include "ha.h"
 #include "hvac.h"
+#include "init.h"
 #include "mqtt.h"
 #include "openthread.h"
 #include "reset.h"
@@ -181,64 +182,6 @@ static int hvac_click_init(hvac_t *hvac)
 	return 0;
 }
 
-static int watchdog_new_channel(const struct device *wdt)
-{
-	int ret;
-
-	struct wdt_timeout_cfg wdt_config = {
-		.window.min = 0,
-		.window.max = (3 * 60 + 40) * MSEC_PER_SEC,
-		.callback = NULL,
-		.flags = WDT_FLAG_RESET_SOC,
-	};
-
-	ret = wdt_install_timeout(wdt, &wdt_config);
-	if (ret < 0) {
-		LOG_ERR("watchdog install error");
-	}
-
-	return ret;
-}
-
-static int watchdog_init(const struct device *wdt,
-			 int *main_channel_id, int *mqtt_channel_id)
-{
-	int ret;
-
-	if (!device_is_ready(wdt)) {
-		LOG_ERR("%s: device not ready", wdt->name);
-		return -ENODEV;
-	}
-
-	ret = watchdog_new_channel(wdt);
-	if (ret < 0) {
-		LOG_ERR("Could not create a new watchdog channel");
-		return ret;
-	}
-
-	*main_channel_id = ret;
-	LOG_INF("main watchdog channel id: %d", *main_channel_id);
-
-	ret = watchdog_new_channel(wdt);
-	if (ret < 0) {
-		LOG_ERR("Could not create a new watchdog channel");
-		return ret;
-	}
-
-	*mqtt_channel_id = ret;
-	LOG_INF("mqtt watchdog channel id: %d", *mqtt_channel_id);
-
-	ret = wdt_setup(wdt, WDT_OPT_PAUSE_HALTED_BY_DBG);
-	if (ret < 0) {
-		LOG_ERR("watchdog setup error");
-		return 0;
-	}
-
-	LOG_INF("🐶 watchdog started!");
-
-	return 0;
-}
-
 static void retry(int (*func)(struct ha_sensor *), struct ha_sensor *sensor)
 {
 	int ret;
@@ -298,7 +241,7 @@ int main(void)
 
 	const struct device *wdt = DEVICE_DT_GET(DT_NODELABEL(wdt0));
 
-	watchdog_init(wdt, &main_wdt_chan_id, &mqtt_wdt_chan_id);
+	init_watchdog(wdt, &main_wdt_chan_id, &mqtt_wdt_chan_id);
 
 	LOG_INF("\n\n🚀 MAIN START 🚀\n");
 
