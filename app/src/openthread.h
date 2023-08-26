7352dc64ded9c6f00a3277dc2049ac884def6810
Francois Gervais <francoisgervais@gmail.com>
2023-08-26 11:46:22 -0400

    Publish public function

--- app/src/openthread.h
+++ app/src/openthread.h
@@ -6,6 +6,7 @@ bool openthread_is_ready();
 void openthread_set_csl_period_ms(int period_ms);
 void openthread_set_low_latency();
 void openthread_set_normal_latency();
+int openthread_erase_persistent_info();
 int openthread_my_start(void);
 int openthread_wait_for_ready(void);
 
